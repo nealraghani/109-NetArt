1b2fb6887123cb2b0086f2a2aa3bf9a9bf19ae8a
nealraghani <nealraghani@gmail.com>
2024-04-07 09:42:33 -0700

    revamped website
    edited pages to be more responsive

---
+++ main.js
@@ -0,0 +1,122 @@
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "hidden";
+    document.querySelector("#image-10").style.visibility = "visible";
+})
+
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden";
+    document.querySelector("#image-3").style.visibility = "hidden";
+    document.querySelector("#image-4").style.visibility = "hidden";
+    document.querySelector("#image-5").style.visibility = "hidden";
+    document.querySelector("#image-6").style.visibility = "hidden";
+    document.querySelector("#image-7").style.visibility = "hidden";
+    document.querySelector("#image-8").style.visibility = "hidden";
+    document.querySelector("#image-9").style.visibility = "hidden";
+})
+
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden";
+})
+
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "hidden";
+})
+
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "hidden";
+})
+
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "hidden";
+})
+
+document.querySelector("#image-6").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "hidden";
+})
+
+document.querySelector("#image-7").addEventListener("click", function(){
+    document.querySelector("#image-7").style.visibility = "hidden";
+})
+
+document.querySelector("#image-8").addEventListener("click", function(){
+    document.querySelector("#image-8").style.visibility = "hidden";
+})
+
+document.querySelector("#image-9").addEventListener("click", function(){
+    document.querySelector("#image-9").style.visibility = "hidden";
+})
+
+// Popup images
+document.querySelector("#image-10").addEventListener("click", function(){
+    document.querySelector("#image-11").style.visibility = "visible";
+})
+
+document.querySelector("#image-11").addEventListener("click", function(){
+    document.querySelector("#image-12").style.visibility = "visible";
+})
+
+document.querySelector("#image-12").addEventListener("click", function(){
+    document.querySelector("#image-13").style.visibility = "visible";
+})
+
+document.querySelector("#image-13").addEventListener("click", function(){
+    document.querySelector("#image-14").style.visibility = "visible";
+})
+
+document.querySelector("#image-14").addEventListener("click", function(){
+    document.querySelector("#image-15").style.visibility = "visible";
+})
+
+document.querySelector("#image-15").addEventListener("click", function(){
+    document.querySelector("#image-16").style.visibility = "visible";
+})
+
+document.querySelector("#image-16").addEventListener("click", function(){
+    document.querySelector("#image-17").style.visibility = "visible";
+})
+
+document.querySelector("#image-17").addEventListener("click", function(){
+    document.querySelector("#image-18").style.visibility = "visible";
+})
+
+document.querySelector("#image-18").addEventListener("click", function(){
+    document.querySelector("#image-10").style.visibility = "hidden";
+    document.querySelector("#image-11").style.visibility = "hidden";
+    document.querySelector("#image-12").style.visibility = "hidden";
+    document.querySelector("#image-13").style.visibility = "hidden";
+    document.querySelector("#image-14").style.visibility = "hidden";
+    document.querySelector("#image-15").style.visibility = "hidden";
+    document.querySelector("#image-16").style.visibility = "hidden";
+    document.querySelector("#image-17").style.visibility = "hidden";
+    document.querySelector("#image-18").style.visibility = "hidden";
+    document.querySelector("#image-19").style.visibility = "visible";
+})
+
+// DM images
+document.querySelector("#image-19").addEventListener("click", function(){
+    document.querySelector("#image-20").style.visibility = "visible";
+})
+
+document.querySelector("#image-20").addEventListener("click", function(){
+    document.querySelector("#image-21").style.visibility = "visible";
+})
+
+document.querySelector("#image-21").addEventListener("click", function(){
+    document.querySelector("#image-22").style.visibility = "visible";
+})
+
+document.querySelector("#image-22").addEventListener("click", function(){
+    document.querySelector("#image-23").style.visibility = "visible";
+})
+
+document.querySelector("#image-23").addEventListener("click", function(){
+    document.querySelector("#image-24").style.visibility = "visible";
+})
+
+document.querySelector("#image-24").addEventListener("click", function(){
+    document.querySelector("#image-25").style.visibility = "visible";
+})
+
+document.querySelector("#image-25").addEventListener("click", function(){
+    document.querySelector("#image-26").style.visibility = "visible";
+})
